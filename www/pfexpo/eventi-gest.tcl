34f97fd51a24e310c85212e68a8717cf1abfdcc3
Mattia Righetti <mattia.righetti@professionefinanza.com>
2016-06-06 14:50:48 +0200

    Sistemata modifica eventi.

--- www/pfexpo/eventi-gest.tcl
+++ www/pfexpo/eventi-gest.tcl
@@ -44,7 +44,10 @@ ad_form -name evento \
 	}
 	{permalink:text
 	    {label "Permalink"}
-	}	    
+	}
+	{short_title:text
+	    {label "Titolo corto"}
+	}
 	{start_time:date,to_sql(sql_date),to_html(sql_date)
 	    {format "DD MONTH YYYY HH24 MI SS"}
 	    {label "Dalle"}
@@ -71,15 +74,15 @@ ad_form -name evento \
 	    {options {{"" t}}}
 	}
     }  -select_query {
-	"SELECT evento_id, denominazione, descrizione, permalink, sala_id, start_time, end_time, prezzo::decimal, percorso_id, soldout FROM expo_eventi WHERE evento_id = :evento_id"
+	"SELECT evento_id, denominazione, short_title, descrizione, permalink, sala_id, start_time, end_time, prezzo::decimal, percorso_id, soldout FROM expo_eventi WHERE evento_id = :evento_id"
     } -new_data {
       	set evento_id [db_string query "SELECT COALESCE(MAX(evento_id)+1,1) FROM expo_eventi"]
 	if {[db_0or1row query "select * from expo_eventi where permalink ilike :permalink limit 1"]} {
 	    append permalink "_2"
 	}
-	db_dml query "INSERT INTO expo_eventi (evento_id, denominazione, permalink, descrizione, sala_id, start_time, end_time, prezzo, expo_id, percorso_id, soldout) VALUES (:evento_id, :denominazione, :permalink, :descrizione, :sala_id, $start_time, $end_time, :prezzo::money, :expo_id, :percorso_id, :soldout)"
+	db_dml query "INSERT INTO expo_eventi (evento_id, denominazione, short_title, permalink, descrizione, sala_id, start_time, end_time, prezzo, expo_id, percorso_id, soldout) VALUES (:evento_id, :denominazione, :short_title, :permalink, :descrizione, :sala_id, $start_time, $end_time, :prezzo::money, :expo_id, :percorso_id, :soldout)"
     } -edit_data {
-	db_dml query "UPDATE expo_eventi SET denominazione = :denominazione, descrizione = :descrizione, sala_id = :sala_id, start_time = $start_time, end_time = $end_time, prezzo = :prezzo::money, permalink = :permalink, soldout = :soldout WHERE evento_id = :evento_id"
+	db_dml query "UPDATE expo_eventi SET denominazione = :denominazione, short_title = :short_title, descrizione = :descrizione, sala_id = :sala_id, start_time = $start_time, end_time = $end_time, prezzo = :prezzo::money, permalink = :permalink, soldout = :soldout WHERE evento_id = :evento_id"
     } -after_submit {
 	ad_returnredirect "eventi-list"
 	ad_script_abort
